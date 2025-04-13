f49c0bb831ef1963dd5fae3d95073fb73fa32c9e
calvinjxie <calvinjxie@gmail.com>
2025-04-12 22:49:51 -0500

    added positions page

--- stock-backtesting-frontend/src/login.js
+++ stock-backtesting-frontend/src/login.js
@@ -6,8 +6,8 @@ document.getElementById('confirm-button').addEventListener('click', () => {
 
     // Simple validation (you can replace this with actual authentication logic)
     if (username && password) {
-        // Redirect to the dashboard page
-        window.location.href = 'dashboard.html';
+        // Redirect to their portfolio page
+        window.location.href = 'portfolio.html';
     } else {
         alert('Please enter both username and password.');
     }
